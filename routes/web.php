12178d8c0bc4b0a9e4e033c4182d25495b65d7d8
aribtm94 <aribtaufiq94@gmail.com>
2025-09-20 01:54:21 +0700

    ganti route gaya breeze

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,36 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    ProductController, CartController, CheckoutController, OrderController
+};
+use App\Http\Controllers\Admin\{ProductAdminController, OrderAdminController};
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [ProductController::class,'index'])->name('home');
+Route::get('/products/{product}', [ProductController::class,'show'])->name('products.show');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/cart', [CartController::class,'index'])->name('cart.index');
+    Route::post('/cart/add/{product}', [CartController::class,'add'])->name('cart.add');
+    Route::patch('/cart/item/{item}', [CartController::class,'updateQty'])->name('cart.update');
+    Route::delete('/cart/item/{item}', [CartController::class,'remove'])->name('cart.remove');
+
+    Route::get('/checkout', [CheckoutController::class,'show'])->name('checkout.show');
+    Route::post('/checkout', [CheckoutController::class,'store'])->name('checkout.store');
+
+    Route::get('/orders', [OrderController::class,'index'])->name('orders.index');
+    Route::get('/orders/{order}', [OrderController::class,'show'])->name('orders.show');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth','can:admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/products', [ProductAdminController::class,'index'])->name('products.index');
+    Route::post('/products', [ProductAdminController::class,'store'])->name('products.store');
+    Route::patch('/products/{product}', [ProductAdminController::class,'update'])->name('products.update');
+    Route::delete('/products/{product}', [ProductAdminController::class,'destroy'])->name('products.destroy');
+
+    Route::get('/orders', [OrderAdminController::class,'index'])->name('orders.index');
+    Route::get('/orders/{order}', [OrderAdminController::class,'show'])->name('orders.show');
+    Route::patch('/orders/{order}/status', [OrderAdminController::class,'updateStatus'])->name('orders.status');
+});
+
+require __DIR__.'/auth.php'; // dari Breeze
