5b6d63e060747cd2cf7c14fdfc1eee7c05cfa900
aribtm94 <aribtaufiq94@gmail.com>
2025-09-20 01:49:13 +0700

    buat gate admin

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,25 +2,12 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
-class AuthServiceProvider extends ServiceProvider
-{
-    /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
-
-    /**
-     * Register any authentication / authorization services.
-     */
-    public function boot(): void
-    {
-        //
+class AuthServiceProvider extends ServiceProvider {
+    public function boot(): void {
+        Gate::define('admin', fn(User $user) => $user->isAdmin());
     }
 }
