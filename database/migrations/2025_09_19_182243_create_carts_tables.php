73be627243e00fd46ebdee39dfaaf853b58e2870
AlfonsoHutahaean <163295212+AlfonsoHutahaean@users.noreply.github.com>
2025-10-15 05:40:32 +0700

    Update 2025_09_19_182243_create_carts_tables.php

--- database/migrations/2025_09_19_182243_create_carts_tables.php
+++ database/migrations/2025_09_19_182243_create_carts_tables.php
@@ -4,38 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
-    {
-
-        Schema::create('orders', function (Blueprint $table) {
+return new class extends Migration {
+    public function up(): void {
+        Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
-            $table->string('order_number', 50)->unique();
-            $table->string('receiver_name', 100);
-            $table->text('receiver_address');
-            $table->string('receiver_phone', 20)->nullable();
-            $table->string('payment_method', 50)->default('transfer');
-            $table->string('status', 50)->default('baru');
-            $table->bigInteger('total')->default(0);
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
 
-        Schema::create('order_items', function (Blueprint $table) {
+        Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
-            $table->foreignId('product_id')->nullable()->constrained('products')->nullOnDelete();
-            $table->bigInteger('price');
-            $table->integer('qty');
-            $table->bigInteger('subtotal');
+            $table->foreignId('cart_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained()->restrictOnDelete();
+            $table->unsignedInteger('qty');
             $table->timestamps();
+
+            $table->unique(['cart_id','product_id']);
         });
     }
-
-    public function down(): void
-    {
-        Schema::dropIfExists('order_items');
-        Schema::dropIfExists('orders');
+    public function down(): void {
+        Schema::dropIfExists('cart_items');
+        Schema::dropIfExists('carts');
     }
 };
