0ce354dce57e2314027f5a82f556edd502b89b6d
AlfonsoHutahaean <163295212+AlfonsoHutahaean@users.noreply.github.com>
2025-10-15 05:29:52 +0700

    Update OrderItem.php

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'price',
+        'qty',
+        'subtotal',
+    ];
+
+    // Relasi ke order
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    // Relasi ke produk
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
